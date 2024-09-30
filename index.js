4fbb6d9ca4bc78137d73651fa2c10462071d2bac
Garrett <garrettbierer@gmail.com>
2024-09-30 10:23:32 -0400

    testing

---
+++ index.js
@@ -0,0 +1,11 @@
+//This is called sudo code
+//Determine board
+//Determine players P1/P2, symbols
+//Put a symbol in a box
+//Player 2 puts the opposite symbol in an empty box
+//P1 goes
+//P2 goes
+//AKA loop through these until someone gets 3 in a row OR board is full
+//Display the results
+
+// test2
